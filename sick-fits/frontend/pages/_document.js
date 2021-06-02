c6864ee554cc43cf1d49715e99fbb402cdbcd245
Kanwaljeet Singh Kalra <koolkalra@gmail.com>
2021-06-02 15:57:10 -0700

    nprogress

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -4,8 +4,8 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang='en'>
-        {/* <Head /> */}
+      <Html lang="en">
+        <Head />
         <body>
           <Main />
           <NextScript />

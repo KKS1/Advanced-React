e45bf6410adf8d2e3acd470a02e4aa5fd411d406
Kanwaljeet Singh Kalra <koolkalra@gmail.com>
2021-06-28 14:01:03 -0700

    Grid layout for Products list

--- sick-fits/frontend/components/Products.jsx
+++ sick-fits/frontend/components/Products.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { gql, useQuery } from '@apollo/client';
+import styled from 'styled-components';
 
 const ALL_PRODUCTS_QUERY = gql`
   # Write your query or mutation here
@@ -19,6 +20,12 @@ const ALL_PRODUCTS_QUERY = gql`
   }
 `;
 
+const ProductsListStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, [col-start] 1fr);
+  grid-gap: 60px;
+`;
+
 export default function Products() {
   const { loading, error, data } = useQuery(ALL_PRODUCTS_QUERY);
 
@@ -28,9 +35,11 @@ export default function Products() {
 
   return (
     <div>
-      {data.allProducts.map((product) => (
-        <p key={product.id}>{product.name}</p>
-      ))}
+      <ProductsListStyles>
+        {data.allProducts.map((product) => (
+          <p key={product.id}>{product.name}</p>
+        ))}
+      </ProductsListStyles>
     </div>
   );
 }
